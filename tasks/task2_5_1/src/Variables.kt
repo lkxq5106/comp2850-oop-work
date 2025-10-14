afa878b5fe59c9f3406432c00bc28208c3468e5a
lkxq5106 <lkxq5106@leeds.ac.uk>
2025-10-14 06:11:02 +0100

    Test GitHub

---
+++ tasks/task2_5_1/src/Variables.kt
@@ -0,0 +1,10 @@
+fun main() {
+    var number = 5
+    println(number)
+
+    // val number = cannot be re-assigned
+    // var number = can be re-assigned
+    number = 10
+
+    println(number)
+}
