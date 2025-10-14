f5a5ba0be58977682fc73e3b28c4ce7b2faf5e90
lkxq5106 <lkxq5106@leeds.ac.uk>
2025-10-14 06:17:42 +0100

    Task 2.5.2

---
+++ tasks/task2_5_2/src/Age.kt
@@ -0,0 +1,10 @@
+fun main() {
+    val myAge = 29u // var
+    val universeAge = 13_800_000_000L // var
+    val status = 'M' // var
+    val name = "Sarah" // val
+    val height = 1.78f // val
+    val root2 = Math.sqrt(2.0) // val
+
+    println(myAge::class)
+}
